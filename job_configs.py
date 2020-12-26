c15bb6235d807b202bf2c3ed993782a3560e1ca0
Xinhong Deng <sandra.deng.0520@gmail.com>
2020-12-26 00:39:39 -0500

    add basic functionalities of slurm

--- job_configs.py
+++ job_configs.py
@@ -1,14 +1,10 @@
 import os
-import getpass
 
-# need to set up the environment variable before use
-ACCOUNT_ID = 'def-dnowrouz-ab'
-USER_NAME = getpass.getuser()
 # change the output directory
 JOB_CONFIG = {
-    'account_id':None,
-    'time': '12:00:00',
+    'account_id': 'def-dnowrouz-ab',
+    'time': '1:00:00',
     'cpus-per-task': '2',
     'mem-per-cpu': '20G',
-    'gres': 'gpu:v100:1',
+    'gres': 'gpu:1',
 }
--- slurm_manager.py
+++ slurm_manager.py
@@ -13,81 +13,116 @@ import requests
 
 # Job submission
 # ==============
-def submit_job(api, account_id, command, job_config, workdir, savedir_logs=None):
-    job_spec = get_job_spec(job_config, command, savedir_logs, workdir=workdir)
-    
+def submit_job(api, account_id, command, job_config, workdir, savedir_logs=None):   
     # read slurm setting
     lines = "#! /bin/bash \n"
-    lines += "#SBATCH --account=%s \n" % job_configs.ACCOUNT_ID
-    # todo: check how the job_spec is defined
-    for key in list(job_spec.JOB_CONFIG.keys()):
-        lines += "#SBATCH --%s=%s \n" % (key, job_spec.JOB_CONFIG[key])
+    lines += "#SBATCH --account=%s \n" % account_id
+    for key in list(job_config.JOB_CONFIG.keys()):
+      lines += "#SBATCH --%s=%s \n" % (key, job_config.JOB_CONFIG[key])
+    path_log = os.path.join(savedir_logs, "logs.txt")
+    path_err = os.path.join(savedir_logs, "err.txt")
+    lines += "#SBATCH --output=%s \n" % path_log
+    lines += "#SBATCH --error=%s \n" % path_err
+    
     lines += command
 
-    exp_id = command.split("-ei ")[1].split(" ")[0]  
-    file_name = "%s.sh" % exp_id
+    file_name = os.path.join(savedir_logs, "bash.sh")
     hu.save_txt(file_name, lines)
+
     # launch the exp
-    submit_command = "sbatch %s.sh" % exp_id
-    job_id = hu.subprocess_call(submit_command).split()[-1]
+    submit_command = "sbatch %s" % file_name
+    while True:
+      try:
+        job_id = hu.subprocess_call(submit_command).split()[-1]
+      except Exception as e:
+        if "Socket timed out" in str(e):
+          print("slurm time out and retry now")
+          time.sleep(1)
+          continue
+      break
 
-    # delete the slurm.sh
+    # delete the bash.sh
     os.remove(file_name)
+
     return job_id
-
-def get_job_spec(job_config, command, savedir_logs, workdir):
-    _job_config = copy.deepcopy(job_config)
-    _job_config['workdir'] = workdir
-    
-    if savedir_logs is not None:
-        path_log = os.path.join(savedir_logs, "logs.txt")
-        path_err = os.path.join(savedir_logs, "err.txt")
-        command_with_logs = '%s 1>%s 2>%s' % (command, path_log, path_err)
-    else:
-        command_with_logs = command
-
-    _job_config['command'] = ['/bin/bash', '-c', command_with_logs]
-    _job_config['resources'] = eai_toolkit_client.JobSpecResources(**_job_config['resources'])
-    job_spec = eai_toolkit_client.JobSpec(**_job_config)
-
-    # Return the Job command in Byte format
-    return job_spec
 
 
 # Job status
 # ===========
 def get_job(api, job_id):
     """Get job information."""
-    try:
-        return api.v1_job_get_by_id(job_id)
-    except ApiException as e:
-        raise ValueError("job id %s not found." % job_id)
+    # todo: use sacct can get the state of completed job, but limited info
+    # sacct header: Account, User, JobID, Start, End, AllocCPUS, Elapsed, AllocTRES, CPUTime, AveRSS, MaxRSS MaxRSSTask MaxRSSNode, NodeList ExitCode, State
+    # todo: return job obj?
+    command = "scontrol show job %s" % (job_id)
+    job_info = ''
+    while True:
+      try:
+        job_info = hu.subprocess_call(command)
+        job_info = job_info.replace('\n', '')
+        job_info = {v.split('=')[0]:v.split('=')[1] for v in job_info.split(' ') if '=' in v }
+      except Exception as e:
+        if "Socket timed out" in str(e):
+          print("scontrol time out and retry now")
+          time.sleep(1)
+          continue
+      break
+    return job_info
+
 
 def get_jobs(api, account_id):
-    # account_id = hu.subprocess_call('eai account get').split('\n')[-2].split(' ')[0]
-    return api.v1_account_job_get(account_id=account_id,
-            limit=1000, 
-            order='-created',
-            q="alive_recently=True").items
-            
-
-    # return api.v1_me_job_get(limit=1000, 
-    #         order='-created',
-    #         q="alive_recently=True").items
+    ''' get all jobs launched by the current user'''
+    # todo: can only get the running and pending jobs; use sacct?
+    # use user_name instead of account_id for the search
+    # todo: return job objs?
+    command = "squeue --user=%s" % getpass.getuser()
+    while True:
+      try:
+        job_list = hu.subprocess_call(command)
+        job_list = job_list.split('\n')
+        job_list = [v.lstrip().split(" ")[0] for v in job_list[1:]]
+        result = []
+        for job_id in job_list:
+          result.append(get_job(None, job_id))
+      except Exception as e:
+            if "Socket timed out" in str(e):
+              print("scontrol time out and retry now")
+              time.sleep(1)
+              continue
+      break
+    return result
            
+
 def get_jobs_dict(api, job_id_list, query_size=20):
-    # get jobs
-    "id__in=64c29dc7-b030-4cb0-8c51-031db029b276,52329dc7-b030-4cb0-8c51-031db029b276"
-
+    # todo: not returning job obj!! error can occur in haven_jobs
+    # info: state, command(?), id, run(?)
+   
+    # todo: get all jobs at once
     jobs = []
-    for i in range(0, len(job_id_list), query_size):
-        job_id_string = "id__in="
-        for job_id in  job_id_list[i:i + query_size]:
-            job_id_string += "%s," % job_id
-        job_id_string = job_id_string[:-1]
-        jobs += api.v1_cluster_job_get(q=job_id_string).items
+    jobs_dict = {}
 
-    jobs_dict = {job.id: job for job in jobs}
+    command = "sacct --jobs=%s" % str(job_id_list)[1:-1].replace(" ", "")
+    job_list = hu.subprocess_call(command)
+    lines = job_list.split('\n')
+    header = lines[0]
+    state_index = header.index("State")
+    job_id_index = header.index("JobID")
+    for line in lines[2:]:
+      job_id = line.split()[job_id_index]
+      if '.' in job_id:
+        continue
+      state = line.split()[state_index]
+      jobs_dict[job_id] = state
+
+
+    # for i in range(0, len(job_id_list)):
+    #     job_id_string = "id__in="
+    #     for job_id in  job_id_list[i:i + query_size]:
+    #         job_id_string += "%s," % job_id
+    #     job_id_string = job_id_string[:-1]
+    #     jobs += api.v1_cluster_job_get(q=job_id_string).items
+
+    # jobs_dict = {job.id: job for job in jobs}
 
     return jobs_dict
     
@@ -97,15 +132,22 @@ def kill_job(api, job_id):
     """Kill a job job until it is dead."""
     job = get_job(api, job_id)
 
-    if not job.alive:
+    # todo: running and pending only?
+    if not job["status"] == "RUNNING" or "PENDING":
         print('%s is already dead' % job_id)
     else:
         # toolkit
-        api.v1_job_delete_by_id(job_id)
-        print('%s CANCELLING...' % job_id)
-        job = get_job(api, job_id)
-        while job.state == "CANCELLING":
-            time.sleep(2.0)
-            job = get_job(api, job_id)
-
+        kill_command  = "scancel %s" % job_id
+        while True:
+          try:
+            hu.subprocess_call(kill_command)
+            print('%s CANCELLING...' % job_id) 
+          except Exception as e:
+            # todo: confirm cacelled state?
+            if "Socket timed out" in str(e):
+              print("scancel time out and retry now")
+              time.sleep(1)
+              continue
+          break
         print('%s now is dead.' % job_id)
+        return
